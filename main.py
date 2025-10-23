4cd09f780d04c28b4ca7dca8e950be1c07085d42
marie <126610455+maru-bor@users.noreply.github.com>
2025-10-23 19:55:21 +0200

    refactor MongoDB connection and update routes
    Refactor MongoDB connection to use environment variables for configuration and update the Flask app to return posts from the database.

--- main.py
+++ main.py
@@ -1,24 +1,35 @@
+from flask import Flask, jsonify, render_template, request
 from pymongo import MongoClient
-from flask import Flask, jsonify, render_template
-from flask import request
+from dotenv import load_dotenv
+import os
 
-
+load_dotenv()
 
 app = Flask(__name__)
 
-client = MongoClient("mongodb://dev.spsejecna.net:27017")
-db = client["webnews"]
-coll = db["idnes"]
+mongo_host = os.getenv("MONGO_HOST", "localhost")
+mongo_port = int(os.getenv("MONGO_PORT", "27017"))
+mongo_db = os.getenv("MONGO_DB", "webnews")
+mongo_collection = os.getenv("MONGO_COLLECTION", "idnes")
+
+mongo_uri = f"mongodb://{mongo_host}:{mongo_port}"
+
+client = MongoClient(mongo_uri)
+db = client[mongo_db]
+coll = db[mongo_collection]
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    posts = list(coll.find({}, {'_id': 0}).sort("date", -1).limit(10))
+    return render_template('index.html', posts=posts)
 
-@app.route('/post', methods = ['GET'])
+
+@app.route('/post', methods=['GET'])
 def get_posts():
-    post = coll.find({'_id' : 0})
-    return post
+    posts = list(coll.find({}, {'_id': 0}))
+    return jsonify(posts)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=False)
+    app.run(host='0.0.0.0', port=8080, debug=False)
